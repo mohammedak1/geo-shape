f9915e3cc5d699740c9a779a8acbf71692597305
mohammedak1 <moali.c137@gmail.com>
2024-06-28 13:32:23 +0400

    fix the mutation problem

--- arena.py
+++ arena.py
@@ -50,14 +50,21 @@ class Arena:
 
     def __mutate(self):
         shape = self.samples.shape
-        mutations = np.random.randint(-10, 2, size=(shape[0], shape[1], shape[3]))
+        mutations = np.random.randint(-70, 2, size=(shape[0], shape[1], shape[3]))
         mutations =  np.where(mutations < -2, 0, np.clip(mutations, -2, 2))
-        big_mutations = np.random.randint(-400, 40, size=(shape[0], shape[1], shape[3]))
+        big_mutations = np.random.randint(-800, 40, size=(shape[0], shape[1], shape[3]))
         big_mutations =  np.where(big_mutations < -40, 0, np.clip(big_mutations, -40, 40))
         self.temp_samples = self.samples + mutations[:, :, np.newaxis, :] + big_mutations[:, :, np.newaxis, :]
 
-        self.temp_samples = np.clip(self.temp_samples, 0, 255)
+        x_overflow_max_pos = np.where(self.temp_samples[:, :, :, 0] > 250)
+        x_overflow_min_pos = np.where(self.temp_samples[:, :, :, 0] < 0)
+        y_overflow_max_pos = np.where(self.temp_samples[:, :, :, 1] > 250)
+        y_overflow_min_pos = np.where(self.temp_samples[:, :, :, 1] < 0)
 
+        self.temp_samples[x_overflow_max_pos[0], x_overflow_max_pos[1], :, 0] -= 20
+        self.temp_samples[x_overflow_min_pos[0], x_overflow_min_pos[1], :, 0] += 20
+        self.temp_samples[y_overflow_max_pos[0], y_overflow_max_pos[1], :, 1] -= 20
+        self.temp_samples[y_overflow_min_pos[0], y_overflow_min_pos[1], :, 1] += 20
 
         
     def __update_if_found_better(self):
@@ -72,8 +79,9 @@ class Arena:
         for index in top_n_indices:
             sample = self.temp_samples[index]
             arrays.append(np.full((shape[0] // TAKE_TOP, shape[1], shape[2], shape[3]), sample))
-        self.top_area = np.mean(top_n_areas)
-        self.samples = self.temp_samples
+        if np.mean(top_n_areas) > self.top_area:
+            self.top_area = np.mean(top_n_areas)
+            self.samples = np.concatenate(arrays)
         
 
     def _evaluate_sample(self, sample):
--- tests/test_arena.py
+++ tests/test_arena.py
@@ -1,14 +1,30 @@
 import unittest
 from arena import Arena
+from shapely import MultiPolygon, Polygon
 
 
 class TEST_ARENA(unittest.TestCase):
-    def test_mutate(self):
-        print("hello")
-        a = Arena() 
-        for i in range(1, 3):
-            print("generation", i)
+    #def test_mutate(self):
+    #    print("hello")
+    #    a = Arena() 
+    #    for i in range(1, 3):
+    #        print("generation", i)
+    #        a.pass_one_generation()
+    #    print(a.get_top_area())
+
+    def test_total_area_should_not_change(self):
+        a = Arena()
+        area_before = MultiPolygon([Polygon(p) for p in a.samples[0]]).area 
+        print(area_before)
+        
+        for _ in range(1, 1000):
             a.pass_one_generation()
-        print(a.get_top_area())
+        
+        area_after = MultiPolygon([Polygon(p) for p in a.samples[0]]).area
+        
+        print("before", area_before, "after", area_after)
+        self.assertEqual(area_before, area_after)
+
+         
         
         
